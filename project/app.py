8e00ce901423dc02057b5f4d9359b7052856276e
davidtnfsh <davidtnfsh@gmail.com>
2020-08-14 12:20:47 +0800

    feat(dashboard): finish api of dashboard

--- project/app.py
+++ project/app.py
@@ -4,9 +4,10 @@ entrypoint of falcon
 import falcon
 
 from project.database import SESSION
-from project.resources import PiggyResource
+from project.resources import PiggyResource, DashBoardResource
 from project.middleware import ConnectionManager
 
 app = application = falcon.API(middleware=[ConnectionManager(SESSION)])
 
 app.add_route("/settings", PiggyResource())
+app.add_route("/dashboard", DashBoardResource())
--- project/resources.py
+++ project/resources.py
@@ -11,7 +11,6 @@ class PiggyResource:
     """
     A resource for SMARTAGRI INTEGRATION SERVICE CO., LTD.
     """
-
     def on_get(self, _, resp) -> None:
         """
         For health check probes.
@@ -25,3 +24,21 @@ class PiggyResource:
         with open(Path(req.media["filepath"]).joinpath("settings.json"), "w") as file:
             json.dump(req.media["payload"], file)
         resp.status = falcon.HTTP_201
+
+class DashBoardResource:
+    """
+    A resource for dashboard
+    """
+
+    def on_get(self, req, resp) -> None:
+        """
+        Get records from DB
+        """
+        result = []
+        keys = req.context["sess"].execute('SELECT * FROM ODS_FARM_ID_TIMESTAMP;').keys()
+        for values in req.context["sess"].execute('SELECT * FROM ODS_FARM_ID_TIMESTAMP;'):
+            payload = dict(zip(keys, values))
+            payload['TIMESTAMP'] = str(payload['TIMESTAMP'])
+            del payload['IMAGE_BLOB']
+            result.append(payload)
+        resp.media = result
