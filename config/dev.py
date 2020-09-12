4bb46d5f17ecb23c52f28b1fbf46037455fc6ee6
davidtnfsh <davidtnfsh@gmail.com>
2020-09-12 11:17:31 +0800

    feat(config): add env settings in docker-compose.yml

--- config/dev.py
+++ config/dev.py
@@ -3,6 +3,7 @@ config of dev
 """
 from kafka import KafkaProducer
 import json
+import os
 class Pipeline():
     def __init__(self):
         self.output = [None]
@@ -11,8 +12,8 @@ class Pipeline():
         self.output[0] = frame
 
 PIPELINE = Pipeline()
-EMAIL_OF_SENDER = 'davidtnfsh.dt42@gmail.com'
-EMAIL_OF_RECEIVER = 'davidtnfsh.dt42@gmail.com'
+EMAIL_OF_SENDER = os.getenv("EMAIL_OF_SENDER", 'davidtnfsh.dt42@gmail.com')
+EMAIL_OF_RECEIVER = os.getenv("EMAIL_OF_RECEIVER", 'davidtnfsh.dt42@gmail.com')
 KAFKA_CONFIG = {
     "producer": KafkaProducer(bootstrap_servers=['host.docker.internal:9092', '172.17.0.1:9092'],
                          value_serializer=lambda x: 
--- config/prod.py
+++ config/prod.py
@@ -3,6 +3,7 @@ config of prod
 """
 from kafka import KafkaProducer
 import json
+import os
 class Pipeline():
     def __init__(self):
         self.output = [None]
@@ -21,8 +22,8 @@ PIPELINE = Pipeline()
 #     verbosity=0,
 #     lab_flag=False,
 # )
-EMAIL_OF_SENDER = ''
-EMAIL_OF_RECEIVER = ''
+EMAIL_OF_SENDER = os.getenv("EMAIL_OF_SENDER", '')
+EMAIL_OF_RECEIVER = os.getenv("EMAIL_OF_RECEIVER", '')
 KAFKA_CONFIG = {
     "producer": KafkaProducer(bootstrap_servers=['host.docker.internal:9092', '172.17.0.1:9092'],
                          value_serializer=lambda x: 
