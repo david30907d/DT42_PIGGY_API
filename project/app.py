3a6d4a66fd6bdd6aaa36f30287dcd48733775346
davidtnfsh <davidtnfsh@gmail.com>
2020-10-12 21:03:49 +0800

    fix(merge): remove merge part

--- project/app.py
+++ project/app.py
@@ -12,15 +12,7 @@ from project.resources import (
 )
 from project.middleware import ConnectionManager
 
-if os.getenv("LOCATION") in ("dev", "pytest"):
-    from falcon_cors import CORS
-
-    cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True,)
-    app = application = falcon.API(
-        middleware=[ConnectionManager(SESSION), cors.middleware]
-    )
-else:
-    app = application = falcon.API(middleware=[ConnectionManager(SESSION)])
+app = application = falcon.API(middleware=[ConnectionManager(SESSION)])
 app.add_route("/settings", PiggyResource())
 app.add_route("/dashboard", DashBoardResource())
 app.add_route("/videofeed", VideoResource())
